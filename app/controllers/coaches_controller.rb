f9b26754d4721c1249167196cae714a86a668aeb
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-03 13:16:01 -0500

    add include time slots to coach index action

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,6 +1,6 @@
 class CoachesController < ApplicationController
   def index
     coaches = Coach.all
-    render json: coaches.as_json
+    render json: coaches, include: ['time_slots']
   end
 end
