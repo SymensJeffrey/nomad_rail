c72c91520998779f195cbbf4777ca59cd467c2ea
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-03 13:03:54 -0500

    add coach index action

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,2 +1,6 @@
 class CoachesController < ApplicationController
+  def index
+    coaches = Coach.all
+    render json: coaches.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/coaches" => "coaches#index"
+  
   patch "/time_slots/:id" => "time_slots#update"
 end
