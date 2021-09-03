3aeb4e545f01241ca9ecf4f33a797c55b36c5604
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-03 13:00:32 -0500

    add time slot update action

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,2 +1,11 @@
 class TimeSlotsController < ApplicationController
+
+  def update
+    time_slot = TimeSlot.find_by(id:params[:id])
+    if time_slot.status == "open"
+      time_slot.status = "booked"
+    end
+    time_slot.save
+    render json:{message: "You booked this time slot:"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  patch "/time_slots/:id" => "time_slots#update"
 end
